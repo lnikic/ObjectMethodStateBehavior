8fdbae8c135d1e4a871b65e00d9af69281fda0a6
nikic <nikic.luka2001@gmail.com>
2025-11-04 19:29:34 +0100

    object method diferent behavior

--- src/Dog.java
+++ src/Dog.java
@@ -1,3 +1,14 @@
 public class Dog {
+    String name;
+    int size;
 
+    void bark() {
+        if (size > 60) {
+            System.out.println(name + " says" + " Wooof! Wooof!");
+        } else if (size > 14) {
+            System.out.println("Ruff! Ruff!");
+        } else {
+            System.out.println("Yip! Yip!");
+        }
+    }
 }
